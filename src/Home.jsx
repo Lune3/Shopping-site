02e530b6c11cd2d0651cc70d79e20c317c9c15c9
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-07-15 23:29:46 +0530

    Font changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
                 <h2>Latest Collection</h2>
                 <h1>GET 20% OFF ON ALL TROUSERS - LIMITED TIME ONLY</h1>
                 <p>From tailored classic to contemporary essentials, we have a pair for every occasion</p>
-                <button><Link to={"/shop"}> Shop </Link></button>
+                <button><Link to={"/shop"}> Shop Now </Link></button>
             </div>
         </div>
     )
