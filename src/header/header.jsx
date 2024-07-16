4504b209b737a74445272133576058f6f0e7f784
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-07-16 23:21:43 +0530

    image src change to public

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -13,14 +13,14 @@ function Header({itemAmount}){
                 <input type="text" placeholder="Search for a product"/>
                 <ul className="icons">
                     <li>
-                        <img src="src/assets/heart-svgrepo-com.svg"/>
+                        <img src="assets/heart-svgrepo-com.svg"/>
                     </li>
                     <li className="cart">
                         <div key={itemAmount} style={{backgroundColor:itemAmount > 0 ?"black":""}} className="cartItems">{itemAmount > 0 ? itemAmount:""}</div>
-                        <img src="src/assets/shopping-bag-svgrepo-com.svg"></img>
+                        <img src="assets/shopping-bag-svgrepo-com.svg"></img>
                     </li>
                     <li>
-                        <img src="src/assets/user-rounded-svgrepo-com.svg"></img>
+                        <img src="assets/user-rounded-svgrepo-com.svg"></img>
                     </li>
                 </ul>
            </div>
